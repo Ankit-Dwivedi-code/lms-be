0e9a8f24d85aa54799d0e9bae5ecad390518c252
satish-aquimo <skumar@aquimo.com>
2024-12-08 23:51:25 +0530

    razorpay integrated

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,10 +1,17 @@
-import express from 'express';
-import { processPayment } from '../controllers/payment.controller.js';
-import { VerifyStudent } from "../middlewares/auth.middleware.js"
+import express from "express";
+import {
+  processPayment,
+  initializePayment,
+  verifyPayment,
+} from "../controllers/payment.controller.js";
+import { VerifyStudent } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 // Protected route to process payment and enroll student
-router.route('/purchase-course/:courseId').post(VerifyStudent, processPayment)
+router.route("/purchase-course/:courseId").post(VerifyStudent, processPayment);
+
+router.route("/initialize-payment").post(initializePayment);
+router.route("/verify-payment").post(verifyPayment);
 
 export default router;
