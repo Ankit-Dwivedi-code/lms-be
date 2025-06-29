a1a456c163f869227121f7124b62a9055ac9ec99
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2025-06-29 21:15:35 +0530

    Remove user agent setting and unnecessary wait for body selector in getWebsiteContent function

--- src/controllers/scrapeData.controller.js
+++ src/controllers/scrapeData.controller.js
@@ -26,13 +26,8 @@ async function getWebsiteContent() {
         // Scrape new content if not found in cache
         const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
-         await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-    );
 
         await page.goto(url, { waitUntil: "networkidle2" });
-
-        await page.waitForSelector("body");
 
         const content = await page.evaluate(() => document.body.innerText);
         await browser.close();
